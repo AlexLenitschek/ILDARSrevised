c6837030644c2d21e3c277ca91ae5def39bf305f
Milan Müller <milan@milofsenpai.com>
2022-11-16 10:36:36 +0100

    move evaulation to the package again

--- evaluation/__main__.py
+++ evaluation/__main__.py
@@ -20,3 +20,16 @@ receiver_position = (0.1, 0.1, 0.1)
 sender_positions = [(random.uniform(-1,1), random.uniform(-1,1), random.uniform(-1,1)) for i in range(10)]
 
 (direct_signals, reflected_signals) = signal_simulation.generate_measurements(receiver_position,sender_positions,testrooms.CUBE)
+
+### Debugging visualization
+import vedo
+import os
+import numpy as np
+
+room = vedo.Mesh(os.getcwd() + "/evaluation/testrooms/models/cube.obj").wireframe()
+direct_signal_arrows = [
+    vedo.Arrow(receiver_position, np.add(receiver_position, direct_signal.direction),s=0.002).color("red")
+    for direct_signal in direct_signals]
+reflected_signal_arrows = [vedo.Arrow(receiver_position, np.add(receiver_position, reflected_signal.direction),s=0.002).color("blue") for reflected_signal in reflected_signals]
+
+vedo.show(room, direct_signal_arrows, reflected_signal_arrows)
