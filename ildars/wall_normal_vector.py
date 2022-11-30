3a3c6f05b39d8bee846e249f4dd1cffb78a07124
Milan Müller <milan@milofsenpai.com>
2022-11-30 10:24:01 +0100

    Boilerplate file for wall normal vector computation

---
+++ ildars/wall_normal_vector.py
@@ -0,0 +1,27 @@
+### Contains all methods for wall normal vector computation
+# Functions take as input a set of measurements (here we use the ReflectedSignal class)
+# it is assumend that all given measurements belong to the same wall.
+
+def compute_wall_normal_vector_all_pairs(reflected_signals):
+    # TODO: implement
+    # TODO: assign computed wall normal vector to each reflected signal, i.e
+    # reflected_signal.wall_normal_vector = computed_wall_normal_vector
+    print("All Pairs not yet implemented")
+
+def compute_wall_normal_vector_all_pairs_linear(reflected_signals):
+    # TODO: implement
+    # TODO: assign computed wall normal vector to each reflected signal, i.e
+    # reflected_signal.wall_normal_vector = computed_wall_normal_vector
+    print("Linear All Pairs not yet implemented")
+
+def compute_wall_normal_vector_overlapping_pairs(reflected_signals):
+    # TODO: implement
+    # TODO: assign computed wall normal vector to each reflected signal, i.e
+    # reflected_signal.wall_normal_vector = computed_wall_normal_vector
+    print("Overlapping Pairs not yet implemented")
+
+def compute_wall_normal_vector_disjoint_pairs(reflected_signals):
+    # TODO: implement
+    # TODO: assign computed wall normal vector to each reflected signal, i.e
+    # reflected_signal.wall_normal_vector = computed_wall_normal_vector
+    print("Disjoint Pairs not yet implemented")
