b47e23bebfeebbb15bc271585c3de198268a49f2
Milan Müller <milan@milofsenpai.com>
2022-12-04 18:10:18 +0100

    clean up main file

--- evaluation/__main__.py
+++ evaluation/__main__.py
@@ -17,16 +17,3 @@ reflection_clusters = ildars.run_ildars(
     ildars.WallNormalAlgorithm.ALL_PAIRS, 
     ildars.WallSelectionMethod.LARGEST_REFLECTION_CLUSTER,
     ildars.LocalizationAlgorithm.CLOSEST_LINES)
-
-### Debugging visualization
-# import vedo
-# import os
-# import numpy as np
-
-# room = vedo.Mesh(os.getcwd() + "/evaluation/testrooms/models/cube.obj").wireframe()
-# direct_signal_arrows = [
-#     vedo.Arrow(receiver_position, np.add(receiver_position, direct_signal.direction),s=0.002).color("red")
-#     for direct_signal in direct_signals]
-# reflected_signal_arrows = [vedo.Arrow(receiver_position, np.add(receiver_position, reflected_signal.direction),s=0.002).color("blue") for reflected_signal in reflected_signals]
-
-# vedo.show(room, direct_signal_arrows, reflected_signal_arrows)
