9fd90210e7092adb3abcae790d656c0f6691d07b
Milan Müller <mail@milanmueller.de>
2023-01-09 19:31:25 +0100

    initial error simulation setup

--- evaluation/error_simulation.py
+++ evaluation/error_simulation.py
@@ -1,7 +1,10 @@
 """Functions for simulating error on previously generated measurements
 """
+
+import random
 import numpy as np
 from scipy.stats import vonmises
+from scipy.spatial.transform import Rotation
 
 def simulate_reflection_error(reflected_signals, von_mises_error, delta_error, wall_error):
     for signal in reflected_signals:
@@ -15,15 +18,21 @@ def simulate_reflection_error(reflected_signals, von_mises_error, delta_error, w
 
 def simulate_directional_error(vector, von_mises_error):
     orthogonal_vector = random_orthogonal_vector(vector)
-    return vector
+    random_angle = vonmises(von_mises_error).rvs()
+    # rotate using the random orthogonal vector as rotation vector
+    rotation = Rotation.from_rotvec(random_angle * orthogonal_vector)
+    return rotation.apply(vector)
 
 def random_orthogonal_vector(vector):
     normalized_vector = np.divide(vector, np.linalg.norm(vector))
     rearranged_vector = np.array([-1 * normalized_vector[2], normalized_vector[0], normalized_vector[1]])
     tangent = np.cross(normalized_vector, rearranged_vector)
     bitangent = np.cross(normalized_vector, tangent)
-    random_angle = vonmises.rvs(1)
-    return vector
+    random_angle = random.uniform(-np.pi, np.pi) # use built-in uniform distribution because scipy's vonmises does not support concentration of 0
+    return np.add(
+        np.multiply(tangent, np.sin(random_angle)), 
+        np.multiply(bitangent, np.cos(random_angle))
+        )
 
 def simulate_numeric_error(delta, delta_error):
     return delta
