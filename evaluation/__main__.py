35a043fcdbf61215e5fbd9eb3dbea5af0d120754
Milan Müller <mail@milanmueller.de>
2023-01-11 09:37:32 +0100

    visualize results

--- evaluation/__main__.py
+++ evaluation/__main__.py
@@ -1,8 +1,11 @@
+import os
+import random
+import vedo
 import ildars
+
 from . import testrooms
 from . import signal_simulation
 from . import error_simulation
-import random
 
 # Error setup
 VON_MISES_CONCENTRATION = 1 # concentration of 1 lead so uniform distribution
@@ -14,7 +17,8 @@ receiver_position = (0.1, 0.1, 0.1)
 # random sender positions
 sender_positions = [(random.uniform(-1,1), random.uniform(-1,1), random.uniform(-1,1)) for i in range(10)]
 
-(direct_signals, reflected_signals) = signal_simulation.generate_measurements(receiver_position,sender_positions,testrooms.CUBE)
+(direct_signals, reflected_signals) = signal_simulation.generate_measurements(receiver_position, sender_positions, testrooms.CUBE)
+# TODO: fix and use error simulation
 # reflected_signals = error_simulation.simulate_reflection_error(reflected_signals, VON_MISES_CONCENTRATION, DELTA_ERROR, WALL_ERROR)
 
 ### For now: hardcoded inversion based clustering. In the future, evaulate all implemented algorithms of the ILDARS pipeline here
@@ -26,4 +30,15 @@ computed_sender_positions = ildars.run_ildars(
     ildars.localization.WallSelectionMethod.LARGEST_REFLECTION_CLUSTER,
     ildars.localization.LocalizationAlgorithm.WALL_DIRECTION)
 
-print("ildars computed the following sender positions:", computed_sender_positions)
+### Plot room alongside the actual and computed sender positions
+mesh_room = vedo.Mesh(os.getcwd() + "/evaluation/testrooms/models/cube.obj").wireframe()
+mesh_positions_original = [vedo.Point(position_original, c="red") for position_original in sender_positions]
+mesh_positions_computed = [vedo.Point(position_computed, c="blue") for position_computed in computed_sender_positions]
+mesh_axes = vedo.Axes(mesh_room, xrange = (-1.5, 1.5), yrange = (-1.5, 1.5), zrange = (-1.5, 1.5))
+vedo.show(
+    mesh_room, 
+    mesh_positions_original,
+    mesh_positions_computed,
+    mesh_axes,
+    camera={'pos': (5,5,5), 'viewup': (0,0,1), 'focal_point': (0,0,0)},
+).close()
