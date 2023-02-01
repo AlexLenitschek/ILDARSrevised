1a491b5099c1c35e6faf07ff0b2a501655aa710a
Milan Müller <mail@milanmueller.de>
2023-02-01 10:27:09 +0100

    basic dynamic visualization

--- evaluation/__main__.py
+++ evaluation/__main__.py
@@ -1,103 +1,57 @@
 import random
-import os
-
-import vedo
 
 import ildars
 from . import testrooms
 from . import signal_simulation
+from .visualization import Visualizer
 
 # Error setup
 VON_MISES_CONCENTRATION = 1  # concentration of 1 lead so uniform distribution
 DELTA_ERROR = 0.1  # 10cm standard deviation
 WALL_ERROR = 0  # no wrongly assigned reflections
 
-
 receiver_position = (0.1, 0.1, 0.1)
-# random sender positions
-sender_positions = [
-    (random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1))
-    for i in range(10)
-]
 
-(direct_signals, reflected_signals) = signal_simulation.generate_measurements(
-    receiver_position, sender_positions, testrooms.CUBE
-)
-# TODO: fix and use error simulation
-# reflected_signals = error_simulation.simulate_reflection_error(
-#     reflected_signals, VON_MISES_CONCENTRATION, DELTA_ERROR, WALL_ERROR
-# )
 
-# For now: hardcoded inversion based clustering. In the future, evaulate
-# all implemented algorithms of the ILDARS pipeline here
-clusters, positions = ildars.run_ildars(
-    direct_signals,
-    reflected_signals,
-    ildars.clustering.ClusteringAlgorithm.INVERSION,
-    ildars.walls.WallNormalAlgorithm.ALL_PAIRS,
-    ildars.localization.WallSelectionMethod.LARGEST_REFLECTION_CLUSTER,
-    ildars.localization.LocalizationAlgorithm.WALL_DIRECTION,
-)
+def run_experiment():
+    # random sender positions
+    sender_positions = [
+        (random.uniform(-1, 1), random.uniform(-1, 1), random.uniform(-1, 1))
+        for i in range(10)
+    ]
 
-# Plot room alongside the actual and computed sender positions
-mesh_room = vedo.Mesh(
-    os.getcwd() + "/evaluation/testrooms/models/cube.obj"
-).wireframe()
-
-mesh_positions_original = [
-    vedo.Point(position_original, c="red")
-    for position_original in sender_positions
-]
-mesh_positions_computed = [
-    vedo.Point(position_computed, c="blue") for position_computed in positions
-]
-mesh_normals = [
-    vedo.Arrow(
-        start_pt=receiver_position,
-        end_pt=(receiver_position + cluster.wall_normal),
-        s=0.002,
-        c="green",
+    (
+        direct_signals,
+        reflected_signals,
+    ) = signal_simulation.generate_measurements(
+        receiver_position, sender_positions, testrooms.CUBE
     )
-    for cluster in clusters
-]
-mesh_axes = vedo.Axes(
-    mesh_room, xrange=(-1.5, 1.5), yrange=(-1.5, 1.5), zrange=(-1.5, 1.5)
+    # TODO: fix and use error simulation
+    # reflected_signals = error_simulation.simulate_reflection_error(
+    #     reflected_signals, VON_MISES_CONCENTRATION, DELTA_ERROR, WALL_ERROR
+    # )
+
+    # For now: hardcoded inversion based clustering. In the future, evaulate
+    # all implemented algorithms of the ILDARS pipeline here
+    clusters, positions = ildars.run_ildars(
+        direct_signals,
+        reflected_signals,
+        ildars.clustering.ClusteringAlgorithm.INVERSION,
+        ildars.walls.WallNormalAlgorithm.ALL_PAIRS,
+        ildars.localization.WallSelectionMethod.LARGEST_REFLECTION_CLUSTER,
+        ildars.localization.LocalizationAlgorithm.WALL_DIRECTION,
+    )
+    return sender_positions, clusters, positions
+
+
+pos_orig, clusters, pos_comp = run_experiment()
+
+
+def new_experiment():
+    pos_orig, clusters, pos_comp = run_experiment()
+    return clusters, pos_orig, pos_comp
+
+
+visualizer = Visualizer(
+    receiver_position, new_experiment, clusters, pos_orig, pos_comp
 )
-
-
-# Functions for visualization
-def show_clusters():
-    print("showing clusters")
-
-
-def show_normals():
-    # hide other meshes
-    for pos_o in mesh_positions_original:
-        pos_o.alpha(0)
-    for pos_c in mesh_positions_computed:
-        pos_c.alpha(0)
-    print("showing normals")
-
-
-def show_positions():
-    print("showing positions")
-
-
-plt = vedo.Plotter()
-plt.add_button(
-    show_clusters, pos=(0.5, 0.1), states=("show reflection clusters", "n")
-)
-plt.add_button(
-    show_normals, pos=(0.5, 0.05), states=("show wall normals", "n")
-)
-plt.add_button(
-    show_positions, pos=(0.5, 0), states=("show sender positions", "n")
-)
-plt.show(
-    mesh_room,
-    mesh_positions_original,
-    mesh_positions_computed,
-    mesh_normals,
-    mesh_axes,
-    camera={"pos": (5, 5, 5), "viewup": (0, 0, 1), "focal_point": (0, 0, 0)},
-).close()
--- evaluation/signal_simulation.py
+++ evaluation/signal_simulation.py
@@ -4,6 +4,7 @@ import ildars
 # "Small" number that the determinant is compared to
 EPSILON = 0.000000001
 
+
 def generate_measurements(receiver_position, sender_positions, room):
     direct_signals = []
     reflected_signals = []
@@ -11,18 +12,32 @@ def generate_measurements(receiver_position, sender_positions, room):
     reflection_index = 0
 
     # collect indices for faces
-    room_triangle_indices = [room.meshes[key].faces for key in room.meshes] 
+    room_triangle_indices = [room.meshes[key].faces for key in room.meshes]
     # Flatten list
-    room_triangle_indices = [item for sublist in room_triangle_indices for item in sublist]
+    room_triangle_indices = [
+        item for sublist in room_triangle_indices for item in sublist
+    ]
     # Compute triangles
-    room_triangles = [[room.vertices[face[0]], room.vertices[face[1]], room.vertices[face[2]]] for face in room_triangle_indices]
+    room_triangles = [
+        [
+            room.vertices[face[0]],
+            room.vertices[face[1]],
+            room.vertices[face[2]],
+        ]
+        for face in room_triangle_indices
+    ]
     for sender_position in sender_positions:
         # Compute direct signal direction and length
-        direct_signal_direction = np.subtract(sender_position, receiver_position)
+        direct_signal_direction = np.subtract(
+            sender_position, receiver_position
+        )
         direct_signal_length = np.linalg.norm(direct_signal_direction)
-        direct_signal_direction = np.divide(direct_signal_direction, np.linalg.norm(direct_signal_direction))
+        direct_signal_direction = np.divide(
+            direct_signal_direction, np.linalg.norm(direct_signal_direction)
+        )
         direct_signal = ildars.DirectSignal(direct_signal_direction)
-        # TODO: Check if the direct signal can actually be receiver or is obstructed by a wall
+        # TODO: Check if the direct signal can actually be receiver or is
+        # obstructed by a wall
         direct_signals.append(direct_signal)
         # compute reflection point for each triangle
         for triangle in room_triangles:
@@ -30,54 +45,86 @@ def generate_measurements(receiver_position, sender_positions, room):
             edge2 = np.subtract(triangle[2], triangle[0])
             normal = np.cross(edge1, edge2)
             line_direction = np.divide(normal, np.linalg.norm(normal))
-            intersection_point = get_line_plane_intersection(receiver_position, line_direction, triangle[0], line_direction)
+            intersection_point = get_line_plane_intersection(
+                receiver_position, line_direction, triangle[0], line_direction
+            )
             if intersection_point is None:
                 # Continue with next triangle, if no intersection exists
                 continue
-            mirror_point = np.add(receiver_position, np.multiply(np.subtract(intersection_point, receiver_position), 2))
-            # Compute and normalize reflection direction and intersect with wall
+            mirror_point = np.add(
+                receiver_position,
+                np.multiply(
+                    np.subtract(intersection_point, receiver_position), 2
+                ),
+            )
+            # Compute and normalize reflection direction and o then intersect
+            # it with walls
             reflection_direction = np.subtract(mirror_point, sender_position)
-            reflection_direction = np.divide(reflection_direction, np.linalg.norm(reflection_direction))
-            reflection_point = get_line_triangle_intersection(sender_position, reflection_direction, triangle)
+            reflection_direction = np.divide(
+                reflection_direction, np.linalg.norm(reflection_direction)
+            )
+            reflection_point = get_line_triangle_intersection(
+                sender_position, reflection_direction, triangle
+            )
             if reflection_point is None:
                 continue
-            reflected_signal_direction = np.subtract(reflection_point, receiver_position)
-            reflected_signal_length = np.linalg.norm(reflected_signal_direction) + np.linalg.norm(np.subtract(sender_position, reflection_point))
-            reflected_signal_direction = np.divide(reflected_signal_direction, np.linalg.norm(reflected_signal_direction))
-            reflected_signals.append(ildars.ReflectedSignal(reflected_signal_direction, direct_signal, reflected_signal_length - direct_signal_length, reflection_index))
+            reflected_signal_direction = np.subtract(
+                reflection_point, receiver_position
+            )
+            reflected_signal_length = np.linalg.norm(
+                reflected_signal_direction
+            ) + np.linalg.norm(np.subtract(sender_position, reflection_point))
+            reflected_signal_direction = np.divide(
+                reflected_signal_direction,
+                np.linalg.norm(reflected_signal_direction),
+            )
+            reflected_signals.append(
+                ildars.ReflectedSignal(
+                    reflected_signal_direction,
+                    direct_signal,
+                    reflected_signal_length - direct_signal_length,
+                    reflection_index,
+                )
+            )
             reflection_index += 1
-    
+
     return (direct_signals, reflected_signals)
 
-def get_line_triangle_intersection(line_point,line_direction,triangle):
+
+def get_line_triangle_intersection(line_point, line_direction, triangle):
     # Check if line l : line_point + x * normal intersects with our triangle,
-    # using the Möller-Trumbore Algorithm as described in https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
+    # using the Möller-Trumbore Algorithm as described in
+    # https://en.wikipedia.org/wiki/M%C3%B6ller%E2%80%93Trumbore_intersection_algorithm
     edge1 = np.subtract(triangle[1], triangle[0])
     edge2 = np.subtract(triangle[2], triangle[0])
     h = np.cross(line_direction, edge2)
     a = np.dot(edge1, h)
-    if (a > -EPSILON and a < EPSILON):
+    if a > -EPSILON and a < EPSILON:
         # Line is parallel to the triangle. Continue with next line
         return None
     f = 1 / a
     s = np.subtract(line_point, triangle[0])
     u = np.multiply(f, np.dot(s, h))
-    if (u < 0 or u > 1):
+    if u < 0 or u > 1:
         # line does not intersect with triangle
         return None
     q = np.cross(s, edge1)
     v = np.multiply(f, np.dot(line_direction, q))
-    if (v < 0 or np.add(u, v) > 1):
+    if v < 0 or np.add(u, v) > 1:
         # line does not intersect with triangle
         return None
-    # now we know that an intersection exists. Next we compute the intersection point
+    # now we know that an intersection exists. Next we compute the
+    # intersection point
     t = np.multiply(f, np.dot(edge2, q))
     intersection_point = np.add(line_point, np.multiply(line_direction, t))
     return intersection_point
 
-def get_line_plane_intersection(line_point, line_direction, plane_point, plane_normal):
-    # Compute the intersection of a line and an infinite plane
-    # base on https://stackoverflow.com/questions/5666222/3d-line-plane-intersection
+
+def get_line_plane_intersection(
+    line_point, line_direction, plane_point, plane_normal
+):
+    # Compute the intersection of a line and an infinite plane based on
+    # https://stackoverflow.com/questions/5666222/3d-line-plane-intersection
     det = np.dot(plane_normal, line_direction)
     if abs(det) > EPSILON:
         w = np.subtract(line_point, plane_point)
