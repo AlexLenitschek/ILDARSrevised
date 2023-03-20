multiple fixes to wall distance computation

evaluation/__main__.py
@@ -88,10 +88,12 @@ def run_experiment(algo_conf, iterations=1):
         print("  iteration:", current_iteration)
         # random sender positions
         sender_positions = [
-            (
-                random.uniform(-1, 1),
-                random.uniform(-1, 1),
-                random.uniform(-1, 1),
+            np.array(
+                [
+                    random.uniform(-1, 1),
+                    random.uniform(-1, 1),
+                    random.uniform(-1, 1),
+                ]
             )
             for i in range(NUM_SENDERS)
         ]

evaluation/signal_simulation.py
@@ -22,21 +22,17 @@ def generate_measurements(receiver_position, sender_positions, room):
     # Compute triangles
     room_triangles = [
         [
-            room.vertices[face[0]],
-            room.vertices[face[1]],
-            room.vertices[face[2]],
+            np.subtract(room.vertices[face[0]], receiver_position),
+            np.subtract(room.vertices[face[1]], receiver_position),
+            np.subtract(room.vertices[face[2]], receiver_position),
         ]
         for face in room_triangle_indices
     ]
     for sender_position in sender_positions:
         # Compute direct signal direction and length
-        direct_signal_direction = np.subtract(
-            sender_position, receiver_position
-        )
+        direct_signal_direction = sender_position
         direct_signal_length = np.linalg.norm(direct_signal_direction)
-        direct_signal_direction = np.divide(
-            direct_signal_direction, np.linalg.norm(direct_signal_direction)
-        )
+        direct_signal_direction = util.normalize(direct_signal_direction)
         direct_signal = ildars.DirectSignal(
             direct_signal_direction, sender_position
         )
@@ -50,17 +46,12 @@ def generate_measurements(receiver_position, sender_positions, room):
             normal = np.cross(edge1, edge2)
             line_direction = util.normalize(normal)
             intersection_point = get_line_plane_intersection(
-                receiver_position, line_direction, triangle[0], line_direction
+                np.zeros(3), line_direction, triangle[0], line_direction
             )
             if intersection_point is None:
                 # Continue with next triangle, if no intersection exists
                 continue
-            mirror_point = np.add(
-                receiver_position,
-                np.multiply(
-                    np.subtract(intersection_point, receiver_position), 2
-                ),
-            )
+            mirror_point = np.multiply(intersection_point, 2)
             # Compute and normalize reflection direction and o then intersect
             # it with walls
             reflection_direction = np.subtract(mirror_point, sender_position)
@@ -70,9 +61,7 @@ def generate_measurements(receiver_position, sender_positions, room):
             )
             if reflection_point is None:
                 continue
-            reflected_signal_direction = np.subtract(
-                reflection_point, receiver_position
-            )
+            reflected_signal_direction = reflection_point
             reflected_signal_length = np.linalg.norm(
                 reflected_signal_direction
             ) + np.linalg.norm(np.subtract(sender_position, reflection_point))

ildars/walls/__init__.py
@@ -37,4 +37,4 @@ def compute_wall_normal_vector(wall_normal_algorithm, reflection_cluster):
         )
     direction = reflection_cluster.wall_normal
     distance = compute_distance(direction, reflection_cluster)
-    reflection_cluster.wall_normal = np.multiply(direction, distance)
+    reflection_cluster.wall_normal = direction * distance

ildars/walls/distance.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+import ildars.math_utils as util
+
 
 def compute_distance(direction, reflection_cluster):
     return sum(
@@ -12,17 +14,20 @@ def compute_distance(direction, reflection_cluster):
 
 def compute_distance_from_measurement(direction, reflection):
     # Compute distance to sender using wall direction formula
-    u = np.divide(direction, np.linalg.norm(direction))
+    u = util.normalize(direction)
     v = reflection.direct_signal.direction
     w = reflection.direction
     delta = reflection.delta
-    b = np.cross(np.cross(u, v), u)
+    b = util.normalize(np.cross(np.cross(u, v), u))
     p = 0
     dot = np.dot(np.subtract(v, w), b)
     if dot != 0:
-        p = np.divide(np.multiply(np.dot(w, b), delta), dot)
+        p = np.abs(np.divide(np.multiply(np.dot(w, b), delta), dot))
+    else:
+        print("encountered parallel vectors in distance averaging")
     # now compute wall distance by projecting (r+s)/2 onto u
-    r = np.multiply(v, p)
-    s = np.multiply(w, np.add(p, delta))
+    r = w * (p + delta)
+    s = v * p
     rshalf = np.divide(np.add(r, s), 2)
-    return np.dot(rshalf, u)
+    wall_distance = np.abs(np.dot(rshalf, u))
+    return wall_distance